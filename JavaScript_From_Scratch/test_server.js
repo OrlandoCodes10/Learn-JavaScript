28b188ce3910867882b932b0ca65b5221004c2ca
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-03 16:15:12 -0500

    Completing simple server

--- JavaScript_From_Scratch/test_server.js
+++ JavaScript_From_Scratch/test_server.js
@@ -8,6 +8,8 @@ let ourApp = http.createServer(function(req, res){
     if (req.url == "/about") {
         res.end("Thank you for the interest in our company")
     }
+
+    res.end("We cannot find the page you are looking for.")
 })
 ourApp.listen(3000)
 
