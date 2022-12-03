d21e64a692194b0623e820b65b3a57ad88167a62
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-02 23:54:48 -0500

    Completing other web pages

--- JavaScript_From_Scratch/test_server.js
+++ JavaScript_From_Scratch/test_server.js
@@ -1,6 +1,13 @@
 let http = require("http")
 
 let ourApp = http.createServer(function(req, res){
-    res.end("Hello, and welcome to our webiste")
+    if (res.url == "/") {
+        res.end("Hello, and welcome to our webiste")
+    }
+
+    if (req.url == "/about") {
+        res.end("Thank you for the interest in our company")
+    }
 })
 ourApp.listen(3000)
+
